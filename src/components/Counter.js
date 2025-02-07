2e2d4f77b1eb3d55d414f68c9b5d14e0a9880057
DiegoMayorga <diegoamayorgat@gmail.com>
2025-02-07 13:20:30 -0500

    Understanding the difference with class-based components and how connect works in this approach. Then, returning to the functional component commenting out this piece of code

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,6 @@
+import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
 
-import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
@@ -30,3 +30,45 @@ const Counter = () => {
 };
 
 export default Counter;
+
+
+/* class Counter extends Component {
+  incrementHandler() { 
+    this.props.increment(); 
+  }
+
+  decrementHandler() {
+    this.props.decrement();
+  }
+
+  toggleCounterHandler() {}
+
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{this.props.counter}</div>
+        <div>
+          <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+        </div>
+        <button onClick={this.toggleCounterHandler.bind(this)}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({ type: "increment" }),
+    decrement: () => dispatch({ type: "decrement" }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter); */
