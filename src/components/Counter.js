f2e771c15da7ed042b45067dfb281a4125b95950
DiegoMayorga <diegoamayorgat@gmail.com>
2025-02-09 18:35:31 -0500

    Toggle Counter button works (such as if it were a global state and being added to the redux file.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,10 +2,13 @@ import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-  const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -22,12 +25,13 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={decrementHandler}>Decrement</button>
         <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={incrementHandler}>Increment</button>
       </div>
+
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
