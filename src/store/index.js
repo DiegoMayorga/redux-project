365096a232df29d7c4b76282082322a8ac4a435b
DiegoMayorga <diegoamayorgat@gmail.com>
2025-02-09 20:08:47 -0500

    Switching if statements to switch case

--- src/store/index.js
+++ src/store/index.js
@@ -6,18 +6,43 @@ import { createStore } from "redux";
 const initialState = { counter: 0, showCounter: false };
 
 const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+        showCounter: state.showCounter,
+      };
+    case "increase":
+      return {
+        counter: state.counter + action.amount,
+        showCounter: state.showCounter,
+      };
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+        showCounter: state.showCounter,
+      };
+    case "toggle":
+      return {
+        showCounter: !state.showCounter,
+        counter: state.counter,
+      };
+
+    default:
+      return state;
+  }
+  /* if (action.type === "increment") {
     return {
       counter: state.counter + 1,
       showCounter: state.showCounter,
     };
   }
 
-  /* 1. if (action.type === "increaseby5") {
+  if (action.type === "increaseby5") {
     return {
       counter: state.counter + 5,
     }
-  } */
+  }
 
   if (action.type === "increase") {
     return {
@@ -40,7 +65,7 @@ const counterReducer = (state = initialState, action) => {
     };
   }
 
-  return state;
+  return state; */
 };
 
 const store = createStore(counterReducer);
